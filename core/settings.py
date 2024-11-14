faa92cb5e49f7d70f23982eecb5c0a0832506352
Michel <michelll1307@uni9.edu.br>
2024-11-14 15:16:00 -0300

    ajustes

--- core/settings.py
+++ core/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mundoimaginario',
+        'USER': 'postgres',
+        'PASSWORD': 'uninove2024',
+        'HOST': 'localhost',
+        'PORT': '5432',
         'USE_TZ': True, 
         'TIME_ZONE': 'America/Sao_Paulo', 
     }
