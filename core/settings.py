ec7f8817024c87325263b83371ec9f8f56daa222
Michel Teixeira da Silva <142738195+MichelWars@users.noreply.github.com>
2024-11-17 12:19:17 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mundoimaginario',
         'USER': 'postgres',
-        'PASSWORD': 'uninove2024',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
         'USE_TZ': True, 
